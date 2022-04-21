40686b7df034fc6b991bf20f07256d821b32f2d7
Aniket1675 <Aniket@05>
2022-04-21 16:39:28 +0530

    ADDED:Welcome to JDBC workshop program

---
+++ src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Scanner;
+/**
+ * Purpose: This class is used for to take input from used perform the operartion
+ * @author Aniket Kumbhar
+ * @version 1.0
+ * @since 21-04-2022
+ */
+
+public class EmployeePayRoll {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*- Welcome to Employee Payroll -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+
+    }
+
+
+}
